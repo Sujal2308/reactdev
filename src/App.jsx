b116343acaa7d7293854f93fb0e04aa7b9ca22d7
Sujal <sujalbhugul08@gmail.com>
2024-04-22 21:24:20 +0530

    Todo App (Basic version)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
 import { useState } from "react";
 import "./App.css";
-import Child from "./components/Child";
 
-function App(){
-  let [myData , setMyData] = useState("")
-  function getData(data){
-    setMyData(data)
+let App = () => {
+  let [displayTask, setdisplayTask] = useState([]);
+
+  function addTask() {
+   let value = document.getElementById("input-val").value;
+   displayTask = [...displayTask , value]
+  //  newArr = [...displayTask,value]
+    setdisplayTask(displayTask);
   }
-  return(
-    <>
-
-    <div className="parentComp">
-      <h1>Parent Component</h1>
-      <p>Value accepted : {myData}</p>
-      <div className="childComp">
-       <Child getData = {getData}/>
-      </div>
-    </div>
-    
-    
-
-    
-    </>
-  )
-
-  }
+    function taskRemover(id) {
+      // console.log(id)
+      let newArr2 = displayTask.filter((item , index) => {
+        return index != id;
+      });
+      setdisplayTask(newArr2);
+    }
  
+  return (
+    <>
+    <center>
+
+    <div className="task-box">
+      <div className="input-sec">
+
+      <input id="input-val" type="text"></input>
+        <button onClick={addTask}>Add</button>
+
+      </div>
+       
+        <br></br>
+        {displayTask.map((items, index) => {
+          return (
+            <div key={index} id="task-container">
+              <p className="tasks" >
+                {items}
+              </p>
+              <p  onClick={()=>taskRemover(index)} className="taskRemover">{"⨯"}</p >
+            </div>
+          );
+        })}
+      </div>
+
+    </center>
+   
+    </>
+  );
+};
 
 export default App;
