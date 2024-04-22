425cf0197c42295806b3329b1c5d20288fff3d19
Sujal <sujalbhugul08@gmail.com>
2024-04-22 22:00:39 +0530

    ShortCircuitEval

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 let App = () => {
-  let [displayTask, setdisplayTask] = useState([]);
-
-  function addTask() {
-   let value = document.getElementById("input-val").value;
-   displayTask = [...displayTask , value]
-  //  newArr = [...displayTask,value]
-    setdisplayTask(displayTask);
-  }
-    function taskRemover(id) {
-      // console.log(id)
-      let newArr2 = displayTask.filter((item , index) => {
-        return index != id;
-      });
-      setdisplayTask(newArr2);
-    }
- 
+  let [demo, setDemo] = useState(1);
   return (
     <>
-    <center>
-
-    <div className="task-box">
-      <div className="input-sec">
-
-      <input id="input-val" type="text"></input>
-        <button onClick={addTask}>Add</button>
-
-      </div>
-       
-        <br></br>
-        {displayTask.map((items, index) => {
-          return (
-            <div key={index} id="task-container">
-              <p className="tasks" >
-                {items}
-              </p>
-              <p  onClick={()=>taskRemover(index)} className="taskRemover">{"⨯"}</p >
-            </div>
-          );
-        })}
-      </div>
-
-    </center>
-   
+      {/* Shortcircuit Evaluation */}
+      {demo && <><Button /></>}
     </>
   );
 };
