1ed1c7a8a8a0e626862e1f037f7957273e191f0f
Sujal <sujalbhugul08@gmail.com>
2024-04-24 23:26:54 +0530

    handling form using usestate and onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import "./App.css";
-import Button from "./components/Button";
 
 let App = () => {
-  let [demo, setDemo] = useState(1);
+  let [userEmail , setUserEmail] = useState("")
+  let [pass , setPass] = useState("")
+  let [userObj , setUserObj] =useState([])
+  function dataSubmit(e){
+    e.preventDefault();
+    //preventing unnessesary rendering of comp on submit the form for fetching data
+    let dataObj = {email : userEmail , password : pass}  
+    setUserObj([...userObj,dataObj])
+    console.log(dataObj)
+    
+  }
   return (
     <>
-      {/* Shortcircuit Evaluation */}
-      {demo && <><Button /></>}
+      
+      <form onSubmit={dataSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email"
+        onChange={(e)=>setUserEmail(e.target.value)}
+        value={userEmail}
+        ></input> <br></br>
+        <label htmlFor="password">Password</label>
+        <input type="text" name="password" id="password"
+        onChange={(e)=>setPass(e.target.value)}
+        value={pass}
+        autoComplete="off"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      {
+        userObj.map((data)=>{
+          return(
+            <>
+              <h1>Welcome {data.email}</h1>
+            </>
+          )
+        })
+      }
     </>
   );
 };
